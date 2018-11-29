3436b2dd5f3c26b97f0ebf8a2bf7f935f1fa5a13
Stephen Lawrence <steve@stevesoffice.com>
2018-11-29 13:35:48 -0800

    lets bump the time key again to test auto-build

--- node/routes/routes.js
+++ node/routes/routes.js
@@ -3,7 +3,7 @@ let appRouter = function (app) {
     app.get("/time", function (req, res) {
         var timeStamp = Math.floor(Date.now() / 1000);
         var requestData = {
-            name: 'time5',
+            name: 'time6',
             value: timeStamp
         };
 
